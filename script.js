d24be6eb02314f5a8bdfc0f85cafbf4175104517
GiaVoNgu <60477747+GiaVoNgu@users.noreply.github.com>
2021-08-02 14:40:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -145,13 +145,13 @@ function submitData(){
 
     fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //mode: 'no-cors', // no-cors, *cors, same-origin
+        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
           'Content-Type': 'application/json'
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
+        //redirect: 'follow', // manual, *follow, error
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     })
     .then (response => response.json())
